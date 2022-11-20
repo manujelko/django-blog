8f110edab85c13b20511bb3127d63a43459b643c
Manuel Šarić <manujelko@gmail.com>
2022-11-20 12:23:07 +0100

    All posts and single posts pages.

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Post
-
-all_posts = []
 
 # Create your views here.
 
@@ -13,9 +11,10 @@ def starting_page(request):
 
 
 def posts(request):
+    all_posts = Post.objects.all().order_by("-date")
     return render(request, "blog/all-posts.html", {"all_posts": all_posts})
 
 
 def post_detail(request, slug):
-    post = next(post for post in all_posts if post["slug"] == slug)
+    post = get_object_or_404(Post, slug=slug)
     return render(request, "blog/post-detail.html", {"post": post})
