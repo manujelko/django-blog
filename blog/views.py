579219b6bfa99a74e44ac84e87b85338c365df96
Manuel Šarić <manujelko@gmail.com>
2022-11-22 17:34:15 +0100

    Add a comment form.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import DetailView, ListView
 
+from .forms import CommentForm
 from .models import Post
+
 
 # Create your views here.
 
@@ -29,4 +31,5 @@ class SinglePostView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["post_tags"] = self.object.tags.all()
+        context["comment_form"] = CommentForm()
         return context
