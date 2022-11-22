e856d13af2651912cd90f884f5ef8793549dd1b2
Manuel Šarić <manujelko@gmail.com>
2022-11-22 20:59:05 +0100

    Read later starting setup.

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.StartPageView.as_view(), name="starting-page"),
     path("posts", views.AllPostsView.as_view(), name="posts-page"),
     path("posts/<slug:slug>", views.SinglePostView.as_view(), name="post-detail-page"),
+    path("read-later", views.ReadLater.as_view(), name="read-later"),
 ]
